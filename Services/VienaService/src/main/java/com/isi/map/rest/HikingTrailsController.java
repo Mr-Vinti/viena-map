2caa0c4650912b069e546b498885e2c8e76c3c21
Marius Vintila <vintila.mariusm@gmail.com>
2021-01-10 21:02:49 +0200

    Added custom predicate
    Co-Authored-By: Emilia Bogozi <73529202+EmiliaBogozi@users.noreply.github.com>
    Co-Authored-By: paulbenge <63068592+paulbenge@users.noreply.github.com>
    Co-Authored-By: Ioana Raluca Petrescu <44580296+ralucapetrescu@users.noreply.github.com>

--- Services/VienaService/src/main/java/com/isi/map/rest/HikingTrailsController.java
+++ Services/VienaService/src/main/java/com/isi/map/rest/HikingTrailsController.java
@@ -6,6 +6,7 @@ import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isi.map.domain.HikingTrails;
@@ -31,9 +32,10 @@ public class HikingTrailsController {
 			@ApiResponse(code = 400, message = "Malformed request"),
 			@ApiResponse(code = 500, message = "Internal error") })
 	@GetMapping
-	public ResponseEntity<List<HikingTrailsDto>> getHikingTrails(@QuerydslPredicate(root = HikingTrails.class) Predicate predicate) {
+	public ResponseEntity<List<HikingTrailsDto>> getHikingTrails(@QuerydslPredicate(root = HikingTrails.class) Predicate predicate,
+			@RequestParam(name = "name:like", required = false) String name) {
 		
-		List<HikingTrailsDto> hikingTrailsDtos = hikingTrailsService.getHikingTrails(predicate);
+		List<HikingTrailsDto> hikingTrailsDtos = hikingTrailsService.getHikingTrails(predicate, name);
 		return ResponseEntity.ok(hikingTrailsDtos);
 	}
 }
--- Services/VienaService/src/main/java/com/isi/map/service/HikingTrailsService.java
+++ Services/VienaService/src/main/java/com/isi/map/service/HikingTrailsService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.isi.map.domain.HikingTrails;
+import com.isi.map.domain.QHikingTrails;
 import com.isi.map.dto.HikingTrailsDto;
 import com.isi.map.repository.HikingTrailsRepository;
+import com.isi.map.util.OptionalBooleanBuilder;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -21,11 +25,16 @@ public class HikingTrailsService {
 
 	HikingTrailsRepository hikingTrailsRepository;
 
-	public List<HikingTrailsDto> getHikingTrails(Predicate predicate) {
-		log.info("Retrieving hiking trails for predicate: {}", predicate.toString());
-		List<HikingTrails> hikingTrails = IterableUtils.toList(hikingTrailsRepository.findAll(predicate));
-		
-		// TODO Auto-generated method stub
+	public List<HikingTrailsDto> getHikingTrails(Predicate predicate, String name) {
+		log.info("Retrieving hiking trails for predicate: {}"
+				+ (StringUtils.isNotBlank(name) ? " and name like: {}" : ""), predicate.toString(), name);
+		QHikingTrails qhikingTrails = QHikingTrails.hikingTrails;
+		BooleanExpression customPredicate = new OptionalBooleanBuilder(qhikingTrails.isNotNull())
+				.notEmptyAnd(qhikingTrails.name::containsIgnoreCase, name).build();
+
+		List<HikingTrails> hikingTrails = IterableUtils.toList(
+				hikingTrailsRepository.findAll(customPredicate != null ? customPredicate.and(predicate) : predicate));
+
 		return hikingTrails.stream().map(HikingTrails::toDto).collect(Collectors.toList());
 	}
 }
